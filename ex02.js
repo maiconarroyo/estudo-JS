31a3eeb9748ed86daf262413931f4331ae0f9b48
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-11-23 16:19:48 -0300

    ex04

---
+++ ex02.js
@@ -0,0 +1,4 @@
+const alvos = ["Ser Ancião","Evangelizadores","Betel"]
+for (const alvo of alvos){
+    console.log(alvo)
+}
---
+++ ex04.js
@@ -0,0 +1,5 @@
+const nome = 'JavaScript';
+
+for (const letra of nome){
+console.log(letra)
+}
