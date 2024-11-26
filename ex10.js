3ceab53add803a1ecd0d657634529f545267295d
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-11-26 17:16:43 -0300

    funções

--- ex10.js
+++ ex10.js
@@ -1,16 +1,23 @@
-let nome = prompt('Digite seu nome: ')
+function calculadora(a,b,op){
+   
 
-function padronizaMensagem(){
-    console.log(`Seja bem vindo ${nome}`);
+    if(op == '+'){
+        return a+b;
+    }else if (op == '-'){
+        return a-b;
+    }else if (op == '/'){
+        return a/b;
+    }else if (op == '*'){
+        return a*b;
+    }
+    else{
+        return console.log("operador invalido");
+    }
+
 }
-padronizaMensagem();
 
-function calculadora(){
-    let a= parseFloat(prompt('Digite o primeiro número'));
-    let b = parseFloat(prompt('Digite o Segundo número'));
-    let op = prompt('Digite o tipo da operação');
+let num1= parseFloat(prompt('Digite o primeiro número'));
+let num2 = parseFloat(prompt('Digite o Segundo número'));
+let operacao = prompt('Digite o tipo da operação');
 
-    console.log(a,b,op);
-}
-
-calculadora();
+console.log(calculadora(2,5,'-'))
